248acab03cb3961d2e8fb3d868c65165223f45db
Dmitriy <khotindima@gmail.com>
2024-08-28 19:35:43 +0300

    new route /user

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,16 @@ exports.getAllUsers = async (req, res) => {
   res.send(posts.rows);
 };
 
+exports.getUser = async (req, res) => {
+  const query = {
+    text: `SELECT * FROM public.user WHERE id=$1`,
+    values: [req.query.id]
+  };
+  const posts = await pool.query(query);
+
+  res.send(posts.rows?.[0] || null);
+};
+
 exports.createUser = async (req, res) => {
   const posts = await pool.query("INSERT INTO users (id, first_name, last_name, username) VALUES (?, ?, ?)");
   res.send(posts.rows);
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const customerController = require('../controllers/user');
 
 router.get('/users', customerController.getAllUsers);
+router.get('/user', customerController.getUser);
 
 module.exports = router;
